ddcaeadf2ba41bea8ecb2d4e3a362941e13332f6
IvanGonzalezRecruiter <100708591+IvanGonzalezRecruiter@users.noreply.github.com>
2022-07-06 09:05:32 -0500

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,175 +1,132 @@
 import React, { useState, useEffect } from "react";
 
+//include images into your bundle
+
 //create your first component
 const Home = () => {
-	const [tareas, setTareas] = useState("");
-	const [listaTareas, setListaTareas] = useState([]);
-
-	const agregarTareas = (e) => {
-		e.preventDefault();
-		let tempLista = [...listaTareas];
-		tempLista.push({ label: tareas, done: false });
-		setListaTareas(tempLista);
-		setTareas("");
-		console.log(tempLista);
-		fetch(
-			"https://assets.breatheco.de/apis/fake/todos/user/IvanGonzalezRecruiter",
-			{
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(tempLista),
-			}
-		)
-			.then((resp) => {
-				console.log("STATUS");
-				console.log(resp.status);
-				return resp.json();
-			})
-			.then((data) => {
-				console.log("ok");
-
-				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-			})
-			.catch((error) => {
-				//manejo de errores
-				console.log(error);
-			});
-	};
-	const eliminarTodo = (e) => {
-		const listaVacia = [];
-		setListaTareas(listaVacia);
-		let tempEliminado = [...listaTareas];
-		console.log(listaTareas);
-		fetch(
-			"https://assets.breatheco.de/apis/fake/todos/user/IvanGonzalezRecruiter",
-			{
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(listaTareas),
-			}
-		)
-			.then((resp) => {
-				console.log("STATUS");
-				console.log(resp.status);
-				return resp.json();
-			})
-			.then((data) => {
-				console.log("ok");
-
-				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-			})
-			.catch((error) => {
-				//manejo de errores
-				console.log(error);
-			});
-	};
-	const handleInputChange = (e) => {
-		setTareas(e.target.value);
-	};
-
-	const eliminarTareas = (indice) => {
-		let listaRemovida = listaTareas.filter(
-			(item, posicion) => posicion !== indice
-		);
-		console.log(listaRemovida);
-		setListaTareas(listaRemovida);
-		fetch(
-			"https://assets.breatheco.de/apis/fake/todos/user/IvanGonzalezRecruiter",
-			{
-				method: "PUT",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(listaRemovida),
-			}
-		)
-			.then((resp) => {
-				console.log("STATUS");
-				console.log(resp.status);
-				return resp.json();
-			})
-			.then((data) => {
-				console.log("console de la data");
-
-				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
-			})
-			.catch((error) => {
-				//manejo de errores
-				console.log(error);
-			});
-	};
-
+	const [todolist, setTodolist] = useState([{ label: "", done: false }]);
+	const [isX, setIsX] = useState(false);
+	const [nuevatarea, setNuevatarea] = useState("");
 	useEffect(() => {
-		fetch(
-			"https://assets.breatheco.de/apis/fake/todos/user/IvanGonzalezRecruiter",
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		)
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/EL225", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
 			.then((resp) => {
-				console.log("STATUS");
-				console.log(resp.status);
-				return resp.json();
+				console.log("get");
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				console.log(resp); // Intentará devolver el resultado exacto como cadena (string)
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
 			})
 			.then((data) => {
 				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
-				setListaTareas(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+				setTodolist(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
 			})
 			.catch((error) => {
 				//manejo de errores
 				console.log(error);
 			});
 	}, []);
+	const handleKeyEnter = (e) => {
+		let newTodo = [...todolist, { label: nuevatarea, done: false }];
+		if (e.key === "Enter") {
+			console.log(newTodo);
+			setTodolist(newTodo);
+			setNuevatarea("");
+			angadido(newTodo);
+		}
+	};
 
+	const borrado = (indice) => {
+		const Ntodo = todolist.filter((item, index) => indice !== index);
+		setTodolist(Ntodo);
+		angadido(Ntodo);
+	};
+
+	const angadido = (nuevoTodolist) => {
+		fetch("http://assets.breatheco.de/apis/fake/todos/user/IvanGonzalezRecruiter", {
+			method: "PUT",
+			body: JSON.stringify(nuevoTodolist),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log("put");
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				console.log(resp); // Intentará devolver el resultado exacto como cadena (string)
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.catch((error) => {
+				//manejo de errores
+				console.log(error);
+			});
+	};
+	const getTareas = () => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/EL225", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then((resp) => {
+				console.log("get");
+				console.log(resp.ok); // Será true (verdad) si la respuesta es exitosa.
+				console.log(resp.status); // el código de estado = 200 o código = 400 etc.
+				console.log(resp); // Intentará devolver el resultado exacto como cadena (string)
+				return resp.json(); // (regresa una promesa) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then((data) => {
+				//Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+				setTodolist(data);
+				console.log(data); //esto imprimirá en la consola el objeto exacto recibido del servidor
+			})
+			.catch((error) => {
+				//manejo de errores
+				console.log(error);
+			});
+	};
 	return (
-		<div className="caja">
-			<div className="titulo mx-auto" style={{ width: "50rem" }}>
-				<h1 className="text-center display-1">qué hay pa' hacer?</h1>
+		<div>
+			<div style={{ marginTop: "10px" }}>
+				<h1 className="text-center">to dos</h1>
 			</div>
-			<div className="card mx-auto" style={{ width: "50rem" }}>
-				<ul className="list-group list-group-flush">
-					<li className="list-group-item d-flex">
-						<form
-							className="me-auto p-2 text-muted fs-4 border-0 container-fluid"
-							onSubmit={agregarTareas}>
-							<input
-								className="border-0 text-muted fs-4 container-fluid"
-								type="text"
-								placeholder="qué hay pa' hacer?"
-								onChange={handleInputChange}
-								value={tareas}></input>
-						</form>
-					</li>
-					{listaTareas.map((element, indice) => (
-						<li className="list-group-item d-flex" key={indice}>
-							<p className="me-auto p-2 text-muted fs-4">
-								{element.label}
-							</p>
+			<div>
+				<input
+					type="text"
+					placeholder="to do"
+					value={nuevatarea}
+					onChange={(e) => {
+						setNuevatarea(e.target.value);
+					}}
+					onKeyDown={handleKeyEnter}></input>
+			</div>
+			<ul className="list-group">
+				{todolist.map((item, index) => (
+					<li
+						onMouseOver={() => setIsX(true)}
+						onMouseLeave={() => setIsX(false)}
+						key={index}
+						className="list-group-item"
+						style={{
+							display: "flex",
+							justifyContent: "space-between",
+						}}>
+						{item.label}
+						{isX && (
 							<button
-								className="btn fs-4 btn-outline-light border-0"
-								onClick={() => eliminarTareas(indice)}>
+								className="btn btn-danger"
+								onClick={() => borrado(index)}>
 								X
 							</button>
-						</li>
-					))}
-				</ul>
-			</div>
-			<button
-				type="button"
-				className="btn btn-success"
-				onClick={() => eliminarTodo()}>
-				Borrar Todo
-			</button>
+						)}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
